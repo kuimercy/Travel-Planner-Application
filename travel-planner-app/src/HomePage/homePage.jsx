bd348c76daa96136919aca7a80764314cfeeb244
Mercy Wanjiru <119761692+kuimercy@users.noreply.github.com>
2024-10-18 12:10:28 +0300

    Update homePage.jsx

--- travel-planner-app/src/HomePage/homePage.jsx
+++ travel-planner-app/src/HomePage/homePage.jsx
@@ -2,7 +2,7 @@
 
 import  { useState } from "react";
 import { Link } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar"; 
+import SearchBar from "../SearchBar/searchBar"; 
 import Itinerary from "../Itinerary/Itinerary"; 
 import { fetchDestinations } from "../Service/apiService"; 
 
